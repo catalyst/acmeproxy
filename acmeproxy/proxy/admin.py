c4ba60c7f4944c7dffbf4e30ac2a69844f7a1fba
Michael Fincham <michael@hotplate.co.nz>
2018-06-26 15:38:43 +1200

    Remove suffix_match

--- acmeproxy/proxy/admin.py
+++ acmeproxy/proxy/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 class AuthorisationAdmin(admin.ModelAdmin):
     list_display = ('name', 'suffix_match', 'created_at', 'created_by_ip')
-    fields = (('name', 'suffix_match'), 'secret')
+    fields = ('name', 'secret')
 
 class ResponseAdmin(admin.ModelAdmin):
     list_display = ('name', 'response', 'created_at', 'expired_at', 'live', 'created_by_ip')
